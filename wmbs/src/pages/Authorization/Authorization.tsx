a9a5e33e689135305706548679eea1aa74368fef
almazongit <ialmaz2001@gmail.com>
2024-08-13 10:33:09 +0500

    Верстка формы логина и пароля для авторизации

---
+++ wmbs/src/pages/Authorization/Authorization.tsx
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import { Button } from '@consta/uikit/Button';
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import { TextField } from '@consta/uikit/TextField';
+import Flex from '@react-css/flex';
+import {Text} from "@consta/uikit/Text";
+import { Layout } from '@consta/uikit/Layout';
+
+interface AuthorizationProps {
+    login: string;
+    password: number;
+}
+
+const Authorization  = (props: AuthorizationProps) => {
+    return (
+            <Flex flexDirection='column' justifyContent='between' alignItems='center'>
+                    <Text>Вход</Text>
+                    <TextField placeholder="Введите логин" type="text" form="defaultBrick" size="m"/>
+                    <TextField placeholder="Введите пароль" />
+                    <Button label="Отправить" />
+            </Flex>
+    );
+}
+
+export default Authorization;
