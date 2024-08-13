a26763a4022bb4f61cd1bed03733c76889d351e8
user <gkamilv@gmail.com>
2024-08-13 21:33:38 +0500

    feat: Исправлено неверное преобразование

--- vlp/src/routers/calculation.py
+++ vlp/src/routers/calculation.py
@@ -8,8 +8,10 @@ router = APIRouter(prefix="/calculation", tags=["calculation"])
 
 @router.post("")
 async def run_calculate(response_data: CalculationRequestSchema):
+    q = response_data.q_values
     response_data.q_values = [i/(10**5) for i in response_data.q_values]
+
     vlp_service = VlpService(response_data)
     x = vlp_service.calculate()
-    y = CalculationResponseSchema(pwf=x, q=response_data.q_values)
+    y = CalculationResponseSchema(pwf=x, q=q)
     return y
