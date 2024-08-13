4cd8affbdf398507b2922c181fb1625b4e7c3986
Хусамов Альберт <a.husamov@yandex.ru>
2024-08-13 20:15:02 +0500

    Подготовлен ApiService

--- wmbs/src/service/ApiService.tsx
+++ wmbs/src/service/ApiService.tsx
@@ -1,4 +1,20 @@
-const ApiService = () => {
+import http from '../http-common'
 
+const ApiService = () => {
+    // const getWellList = () => {
+    //     return http.get<Array>("/well-list");
+    // }
+    // const postField = () => {
+    //     return http.post<Array>("/field");
+    // }
+    // const postWell = () => {
+    //     return http.post<Array>("/well");
+    // }
+    // const getWellData = () => {
+    //     return http.get<Array>("/well-data");
+    // }
+    // const postCalc = () => {
+    //     return http.post<Array>("/calc");
+    // }
 }
-export default ApiService;
+export default ApiService
