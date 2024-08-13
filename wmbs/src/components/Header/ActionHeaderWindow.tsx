074d9e192a48dd6573922d5efb989eeda2b7d9df
almazongit <ialmaz2001@gmail.com>
2024-08-13 12:06:39 +0500

    Добавление окна на выход из пользователя

---
+++ wmbs/src/components/Header/ActionHeaderWindow.tsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import { Button } from '@consta/uikit/Button';
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import { TextField } from '@consta/uikit/TextField';
+import Flex from '@react-css/flex';
+import {Text} from "@consta/uikit/Text";
+import './ActionHeaderWindow.css'
+import { Layout } from '@consta/uikit/Layout';
+
+const ActionHeaderWindow = () => {
+    return
+    <Layout className='header-arrow'>
+        <Flex flexDirection='column' justifyContent='between' alignItems='center'>
+            <Text>Вход</Text>
+            <Button label="Отправить" />
+        </Flex>
+    </Layout>
+   
+}
+
+export default ActionHeaderWindow
