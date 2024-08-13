9dd40fe3aac1dd217fafcc739396173210704f23
Aynur <ainur2205@gmail.com>
2024-08-13 16:27:48 +0500

    исправил косяк в формуле. Добавил Qmax для ответа

--- irp/main.py
+++ irp/main.py
@@ -101,20 +101,25 @@ def calculate(inputData: CalculationInput) -> object:
 
     p_wf = np.arange(StartPwf, EndPwf + StepPwf, StepPwf).tolist()
     Q = []
+    Qmax = 0
     if EndPwf > AverageReservoirePressure:
         raise HTTPException(status_code=422, detail="Input should be less than or equal to AverageReservoirePressure ")
     try:
         PI = (2 * math.pi * Permeability * Thickness) / (
                 FluidViscosity * FluidVoumeFactor * (math.log(SupplyContourRadius / WellRadius) - (1 / 2) + Skin))
+        Qmax = ((PI * (AverageReservoirePressure - SaturationPressure)) / (
+                    1 - 0.2 * (SaturationPressure / AverageReservoirePressure) - 0.8 * (
+                    SaturationPressure / AverageReservoirePressure) ** 2))
         for i in range(len(p_wf)):
             Q.append(((PI * (AverageReservoirePressure - SaturationPressure)) / (
                     1 - 0.2 * (SaturationPressure / AverageReservoirePressure) - 0.8 * (
-                    p_wf[i] / AverageReservoirePressure) ** 2)) * (
+                    SaturationPressure / AverageReservoirePressure) ** 2)) * (
                              1 - 0.2 * (p_wf[i] / AverageReservoirePressure) - 0.8 * (
                              p_wf[i] / AverageReservoirePressure) ** 2))
     except:
         print("Ошибка")
     return {
         'Q': json.dumps(Q),
-        'p_wf': json.dumps(p_wf)
+        'p_wf': json.dumps(p_wf),
+        'Qmax': json.dumps(Qmax)
     }
