2e53eb66bbaa1a711ab0efc3fdbff8571f28d1e7
Хусамов Альберт <a.husamov@yandex.ru>
2024-08-13 22:10:45 +0500

    Добавлены типы данных, прописаны запросы

--- wmbs/src/service/ApiService.tsx
+++ wmbs/src/service/ApiService.tsx
@@ -1,18 +1,25 @@
 import http from '../http-common'
+import IOilfieldData from "../types/oilfield.type";
+import IWellsData from "../types/wells.type";
+import IWellsAndOilfieldData from "../types/wellsAndOilfield.type";
 
 const ApiService = () => {
-    // const getWellList = () => {
-    //     return http.get<Array>("/well-list");
-    // }
-    // const postField = () => {
-    //     return http.post<Array>("/field");
-    // }
-    // const postWell = () => {
-    //     return http.post<Array>("/well");
-    // }
-    // const getWellData = () => {
-    //     return http.get<Array>("/well-data");
-    // }
+    const getOilfield = () => {
+        return http.get<Array<IOilfieldData>>("/oilfield");
+    }
+
+    const postOilfield = () => {
+        return http.post<IOilfieldData>("/oilfield");
+    }
+
+    const postLinkWell = (wellName: string, oilfieldId: string) => {
+        return http.post<Array<IWellsAndOilfieldData>>("/oilfield/linkWell");
+    }
+
+    const getWells = (oilfieldId: string) => {
+        return http.get<Array<IWellsData>>(`${oilfieldId}/wells`);
+    }
+
     // const postCalc = () => {
     //     return http.post<Array>("/calc");
     // }
---
+++ wmbs/src/types/wells.type.ts
@@ -0,0 +1,7 @@
+export default interface IWellsData {
+    id: string,
+    well_number: string,
+    field_id: string,
+    updated_at: Date,
+    created_at: Date
+}
