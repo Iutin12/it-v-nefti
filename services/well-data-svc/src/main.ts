228d4701fb0d68b672745d467e90af58efa11fc6
Baev Mikhail <baev2m@gmail.com>
2024-08-13 23:27:11 +0500

    compose front

--- services/well-data-svc/src/main.ts
+++ services/well-data-svc/src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
+  app.enableCors()
+
   const config = new DocumentBuilder()
     .setTitle('WellData service')
     .setDescription('welldata')
