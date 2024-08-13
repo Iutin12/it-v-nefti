2368c9f25943c9d5977c6aa813e961eea9b79f17
Aynur <ainur2205@gmail.com>
2024-08-13 18:09:22 +0500

    Перевели в систему Си

--- irp/main.py
+++ irp/main.py
@@ -1,7 +1,6 @@
 import json
 import math
 from fastapi import FastAPI, HTTPException
-
 import numpy as np
 from pydantic import BaseModel, Field
 
@@ -11,62 +10,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"neft": "neft"}
-
-
-def params_to_system_c(**args):
-    pass
-
-
-"""
-PI                          - Индекс продуктивности скважины
-Permeability                - Проницаемость пласта
-Thickness                   - Толщина пласта
-FluidViscosity              - Вязкость флюида
-FluidVoumeFactor            - Коэффициент объемного расширения ?? (Объемный коэффициент флюида)
-SupplyContourRadius         - радиус контура питания
-WellRadius                  - радиус скважины
-Skin                        - скин фактор скважины
-
-Q                           - синтетическая кривая IPR c поправкой Вогеля
-AverageReservoirePressure   - среднее пластовое давление
-SaturationPressure          - давление насыщения 
-p_wf                        - забойное давление
-"""
-
-
-# @app.get("/irp/calculate")
-# def calculate(StartPwf: float,
-#               EndPwf: float,
-#               StepPwf: float,
-#               Permeability: float = Query(ge=1, le=100),
-#               Thickness: float = Query(ge=0.1, le=100),
-#               FluidViscosity: float = Query(ge=0.1, le=100),
-#               FluidVoumeFactor: float = Query(ge=1, le=2.1),
-#               SupplyContourRadius: float = Query(ge=100, le=1000),
-#               WellRadius: float = Query(ge=0.05, le=0.3),
-#               Skin: float = Query(ge=-3, le=10),
-#               AverageReservoirePressure: float = Query(ge=100, le=300),
-#               SaturationPressure: float = Query(ge=1, le=100)
-#               ) -> object:
-#     p_wf = np.arange(StartPwf, EndPwf + StepPwf, StepPwf).tolist()
-#     Q = []
-#     if EndPwf > AverageReservoirePressure:
-#         raise HTTPException(status_code=422, detail="Input should be less than or equal to AverageReservoirePressure ")
-#     try:
-#         PI = (2 * math.pi * Permeability * Thickness) / (
-#                 FluidViscosity * FluidVoumeFactor * (math.log(SupplyContourRadius / WellRadius) - (1 / 2) + Skin))
-#         for i in range(len(p_wf)):
-#             Q.append(((PI * (AverageReservoirePressure - SaturationPressure)) / (
-#                     1 - 0.2 * (SaturationPressure / AverageReservoirePressure) - 0.8 * (
-#                     p_wf[i] / AverageReservoirePressure) ** 2)) * (
-#                              1 - 0.2 * (p_wf[i] / AverageReservoirePressure) - 0.8 * (
-#                              p_wf[i] / AverageReservoirePressure) ** 2))
-#     except:
-#         print("Ошибка")
-#     return {
-#         'Q': json.dumps(Q),
-#         'p_wf': json.dumps(p_wf)
-#     }
 
 
 class CalculationInput(BaseModel):
@@ -82,6 +25,23 @@ class CalculationInput(BaseModel):
     Skin: float = Field(ge=-3, le=10)
     AverageReservoirePressure: float = Field(ge=100, le=300)
     SaturationPressure: float = Field(ge=1, le=100)
+
+
+"""
+PI                          - Индекс продуктивности скважины
+Permeability                - Проницаемость пласта (мД -> м^2)
+Thickness                   - Толщина пласта  
+FluidViscosity              - Вязкость флюида ( сП -> Па*с ) ???
+FluidVoumeFactor            - Объемный коэффициент флюида
+SupplyContourRadius         - радиус контура питания
+WellRadius                  - радиус скважины
+Skin                        - скин фактор скважины
+
+Q                           - синтетическая кривая IPR c поправкой Вогеля
+AverageReservoirePressure   - среднее пластовое давление (бар -> Па )
+SaturationPressure          - давление насыщения (бар -> Па)
+p_wf                        - забойное давление (бар -> Па -> атмос)
+"""
 
 
 @app.post("/irp/calculate")
@@ -101,25 +61,33 @@ def calculate(inputData: CalculationInput) -> object:
 
     p_wf = np.arange(StartPwf, EndPwf + StepPwf, StepPwf).tolist()
     Q = []
-    Qmax = 0
+    SecInSut = 86400
+
+    # Конвертируем в м^2
+
+    Permeability = Permeability * (1.02 * 10 ** (-15))
+    # перевод из бар в паскали
+
+    # AverageReservoirePressure *= 10**5
+    # SaturationPressure *= 10**5
+    FluidViscosity /= 1000
     if EndPwf > AverageReservoirePressure:
         raise HTTPException(status_code=422, detail="Input should be less than or equal to AverageReservoirePressure ")
     try:
         PI = (2 * math.pi * Permeability * Thickness) / (
                 FluidViscosity * FluidVoumeFactor * (math.log(SupplyContourRadius / WellRadius) - (1 / 2) + Skin))
-        Qmax = ((PI * (AverageReservoirePressure - SaturationPressure)) / (
-                    1 - 0.2 * (SaturationPressure / AverageReservoirePressure) - 0.8 * (
-                    SaturationPressure / AverageReservoirePressure) ** 2))
+
         for i in range(len(p_wf)):
-            Q.append(((PI * (AverageReservoirePressure - SaturationPressure)) / (
+            Q.append((((PI * (AverageReservoirePressure - SaturationPressure) * 10 ** 5) / (
                     1 - 0.2 * (SaturationPressure / AverageReservoirePressure) - 0.8 * (
                     SaturationPressure / AverageReservoirePressure) ** 2)) * (
-                             1 - 0.2 * (p_wf[i] / AverageReservoirePressure) - 0.8 * (
-                             p_wf[i] / AverageReservoirePressure) ** 2))
+                              1 - 0.2 * ((p_wf[i]) / AverageReservoirePressure) - 0.8 * (
+                              (p_wf[i]) / AverageReservoirePressure) ** 2)) * SecInSut)
     except:
         print("Ошибка")
+    #перевод из баров -> Атмосферы
+    p_wf = map(lambda x: x / 1.013, p_wf)
     return {
-        'Q': json.dumps(Q),
-        'p_wf': json.dumps(p_wf),
-        'Qmax': json.dumps(Qmax)
+        'Q': Q,
+        'p_wf': p_wf
     }
