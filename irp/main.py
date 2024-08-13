8e09141288d62d76c61741a7aa93f09fe96ec6d5
Aynur <ainur2205@gmail.com>
2024-08-13 23:48:21 +0500

    еще исправления

--- irp/main.py
+++ irp/main.py
@@ -39,7 +39,7 @@ SkinFactor                  - скин фактор скважины
 Q                           - синтетическая кривая IPR c поправкой Вогеля
 AverageReservoirePressure   - среднее пластовое давление (бар -> Па )
 SaturationPressure          - давление насыщения (бар -> Па)
-p_wf                        - забойное давление (бар -> Па -> атмос)
+p_wf                        - забойное давление 
 """
 
 
@@ -67,20 +67,19 @@ def calculate(inputData: CalculationInput) -> object:
     Permeability = Permeability * (1.02 * 10 ** (-15))
 
     FluidViscosity /= 1000
-    if EndPwf > AverageReservoirePressure:
-        raise HTTPException(status_code=422, detail="Input should be less than or equal to AverageReservoirePressure ")
-    try:
-        PI = (2 * math.pi * Permeability * Thickness) / (
-                FluidViscosity * FluidVoumeFactor * (math.log(SupplyContourRadius / WellRadius) - (1 / 2) + Skin))
+    PI = (2 * math.pi * Permeability * Thickness) / (
+            FluidViscosity * FluidVoumeFactor * (math.log(SupplyContourRadius / WellRadius) - (1 / 2) + Skin))
 
-        for i in range(len(p_wf)):
+    for i in range(len(p_wf)):
+
+        if p_wf[i] < AverageReservoirePressure:
             Q.append((((PI * (AverageReservoirePressure - SaturationPressure) * 10 ** 5) / (
                     1 - 0.2 * (SaturationPressure / AverageReservoirePressure) - 0.8 * (
                     SaturationPressure / AverageReservoirePressure) ** 2)) * (
                               1 - 0.2 * ((p_wf[i]) / AverageReservoirePressure) - 0.8 * (
                               (p_wf[i]) / AverageReservoirePressure) ** 2)) * SecInSut)
-    except:
-        print("Ошибка")
+        else:
+            Q.append(PI * ((AverageReservoirePressure * 10 ** 5) - p_wf[i]))
 
     return {
         'Q': Q,
