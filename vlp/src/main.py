fa9f77536317e7b7344296b8ceb688fe52179265
user <gkamilv@gmail.com>
2024-08-14 12:20:37 +0500

    feat: Добавлены корсы

--- vlp/src/main.py
+++ vlp/src/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
-from routers import calculate_router
+from vlp.src.routers import calculate_router
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(calculate_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все источники
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8080, reload=True)
