b1910df95c006997f90c98bfea5035b222d790c6
almazongit <ialmaz2001@gmail.com>
2024-08-14 11:15:30 +0500

    исправлена подпись

--- wmbs/src/components/SelectWithAdd/SelectWithAdd.tsx
+++ wmbs/src/components/SelectWithAdd/SelectWithAdd.tsx
@@ -97,7 +97,7 @@ const SelectWithAdd: React.FC<ISelectWithAdd> = (props) => {
         if (valueOilfield && regex.test(valueOilfield)) {
             setIsModalOilfieldOpen(true);
         } else {
-            setErrorOilfield('Введите только буквы кириллицы или латиницы или пробел');
+            setErrorOilfield('Введите только буквы кириллицы или латиницы или цифры');
         }
     };
 
@@ -105,7 +105,7 @@ const SelectWithAdd: React.FC<ISelectWithAdd> = (props) => {
         if (valueWell && regex.test(valueWell)) {
             setIsModalWellOpen(true);
         } else {
-            setErrorWell('Введите только буквы кириллицы или латиницы или пробел');
+            setErrorWell('Введите только буквы кириллицы или латиницы или цифры');
         }
     };
 
