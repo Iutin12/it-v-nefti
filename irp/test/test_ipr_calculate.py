9100a1aa0b2ef5c3567551a37c8d36fe75d6b95d
Aynur <ainur2205@gmail.com>
2024-08-14 08:47:02 +0500

    еще один юнит тест

--- irp/test/test_ipr_calculate.py
+++ irp/test/test_ipr_calculate.py
@@ -21,12 +21,24 @@ def test_calculate_success():
     })
 
     assert response.status_code == 200
-    # data = response.json()
-    # assert "Q" in data
-    # assert "p_wf" in data
-    # assert len(data["Q"]) == 11  # Number of points based on StartPwf, EndPwf, StepPwf
-    # assert len(data["p_wf"]) == 11
 
+def test_calculate_success_long_range():
+    response = client.post("/irp/calculate", json={
+        "StartPwf": 0,
+        "EndPwf": 3000,
+        "StepPwf": 0.01,
+        "Permeability": 50,
+        "Thickness": 10,
+        "FluidViscosity": 1,
+        "FluidVoumeFactor": 1,
+        "SupplyContourRadius": 500,
+        "WellRadius": 0.073,
+        "SkinFactor": 1,
+        "AverageReservoirePressure": 200,
+        "SaturationPressure": 80
+    })
+
+    assert response.status_code == 200
 def test_calculate_invalid_permeability():
     response = client.post("/irp/calculate", json={
         "StartPwf": 0,
@@ -47,7 +59,7 @@ def test_calculate_invalid_permeability():
 
 def test_calculate_invalid_thickness():
     response = client.post("/irp/calculate", json={
-         "StartPwf": 0,
+        "StartPwf": 0,
         "EndPwf": 250,
         "StepPwf": 1,
         "Permeability": 50,
@@ -62,3 +74,5 @@ def test_calculate_invalid_thickness():
     })
 
     assert response.status_code == 422
+
+
